eecb77234f5b82cd1451070ce6ba71d0dd56d37a
epena6656 <epena6656@001LAB102010521.neisd.ad>
2015-04-30 12:28:54 -0500

    asdf

--- Library(Array)/src/ArrayProject.java
+++ Library(Array)/src/ArrayProject.java
@@ -3,66 +3,33 @@ import java.util.Scanner;
 public class ArrayProject {
 
 	public static void main(String[] args) {
-		Scanner stringInput = new Scanner(System.in);
 		Scanner intInput = new Scanner(System.in);
-		System.out.println("Enter the number of games in your library");
-		int NumOfGames = intInput.nextInt();
-		
-		Games games[] = new Games[NumOfGames];
-		for (int x = 0; x<NumOfGames;x++){
-			games[x] = new Games();
-		}
-		for (int index = 0; index < NumOfGames; index++)
-		{
-			System.out.println("Enter Game Name");
-			String name = stringInput.nextLine();
-			System.out.println("Enter Game Genre");
-			String genre = stringInput.nextLine();
-			games[index].add(name, genre);
-			
-		}
-		
-		System.out.println();
-		for (int index = 0; index < NumOfGames; index++)
-		{
-			
-			games[index].showData();
-			
-		}
+		System.out.println("What would you like to do?");
+		System.out.println("1) Checking or Returning a Book");
+		System.out.println("2) Checking or Returning a Movie");
+		System.out.println("3) Checking or Returning Music");
+		int choice1 = intInput.nextInt();
 
+		if (choice1 == 1) {
+
+		} else if (choice1 == 2) {
+
+		} else if (choice1 == 3) {
+
+		} else {
+			System.out.println("That is not a choice");
+		}
 	}
-	
 }
-class Games {
-		
-		private String name;
-		private String genre;
-		
-		public Games()
-		{
-			name="";
-			genre="";
-		}
-		
-		public Games(String n, String g)
-		{
-			add(n,g);
-		}
-		
-		public void add(String n, String g)
-		{
-			
-			name = n;
-			genre = g;
-		}
-		
-		public void showData()
-		{
-			
-			System.out.println("Name: "+name);
-			System.out.println("Genre: "+genre);
-			System.out.println();
-		}
-	}
 
+class Books {
+	private String 
+}
 
+class Movies {
+
+}
+
+class Music {
+
+}
